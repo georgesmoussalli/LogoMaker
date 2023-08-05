7d7a8c51cf30b7e2bb2daa3c48e8ef6573554b2e
Georges Moussalli <georgesmoussalli2106@gmail.com>
2023-08-05 05:18:35 +0200

    All the layouts are good

--- logo_maker/generator.py
+++ logo_maker/generator.py
@@ -8,62 +8,141 @@ from text_object import TextObject
 from icon_object import IconObject
 from layout import apply_layout
 from get_noun_project import get_png_base64
-
+import random
 
 
 _HERE = Path(os.path.abspath(__file__))
 _DIR_DATA = _HERE.parent.parent.joinpath("data")
 
-
-def generate_svg( background : BackgroundObject, title : TextObject ,slogan : TextObject, container_icon : IconObject, center_icon : IconObject, letter_icon : IconObject) -> str :
-
-    template = """
-<svg xmlns='http://www.w3.org/2000/svg' width='{background.width}' height='{background.height}'>
-    <style>
-        @font-face {{
-            font-family: '{title.font}';
-            src: url(data:font/ttf;base64,{title.text_font_data_encoded}) format('truetype');
-        }}
-        @font-face {{
-            font-family: '{slogan.font}';
-            src: url(data:font/ttf;base64,{slogan.text_font_data_encoded}) format('truetype');
-        }}
-    </style>
+def get_most_centered_space_index(text):
+    text_length = len(text)
+    spaces_indices = [i for i in range(text_length) if text[i] == ' ']
     
-    <rect width='{background.width}' height='{background.height}' fill='{background.color}' />
-    <g id="logo-group">
-        <image href="data:image/png;base64,{center_icon.png_base64}" x='{center_icon.x}' y='{center_icon.y}' width='{center_icon.width}' height='{center_icon.height}' transform=" scale({letter_icon.scale})"/>
-        <g id="title-container" transform="translate({title.group_x},{title.group_y})">   
-            <g id="title" style="font-size:{title.font_size};font-family:'{title.font}'">
-            """
-    template += tab[1]
+    # return index of space that is closest to the center of the string
+    return min(spaces_indices, key=lambda index: abs(text_length / 2 - index))
+
+def title_splitter(title: TextObject, title_1 : TextObject, title_2 : TextObject):
+
+    if ' ' not in title.content:
+        # if there's no space in the title, return False and the original title objects
+        return False
+    else:
+        split_flag = True #random.choice([True, False])
+        
+        if not split_flag:
+            # if split flag is False, return False and the original title objects
+            return False, title, title
+        else:
+            # split the title content at the most centered space
+            split_index = get_most_centered_space_index(title.content)
+            title_1.content, title_2.content = title.content[:split_index], title.content[split_index+1:]
+            
+            # adjust the position of the titles if needed
+            # title_1.y += 10  # for example
+            # title_2.y -= 10  # for example
+
+            # return True and the new title objects
+            return True
+
+
+
+def generate_svg( background : BackgroundObject, title : TextObject ,slogan : TextObject, container_icon : IconObject, center_icon : IconObject, letter_icon : IconObject, splitted : bool, title_1 : TextObject, title_2 : TextObject) -> str :
+
+    if(splitted == False) : 
+        template = """
+        <svg xmlns='http://www.w3.org/2000/svg' width='{background.width}' height='{background.height}'>
+             <style>
+                @font-face {{
+                    font-family: '{title.font}';
+                    src: url(data:font/ttf;base64,{title.text_font_data_encoded}) format('truetype');
+                 }}
+                @font-face {{
+                    font-family: '{slogan.font}';
+                    src: url(data:font/ttf;base64,{slogan.text_font_data_encoded}) format('truetype');
+                }}
+            </style>
     
-    template += """</g>
-        </g>
-        <g id="slogan-container" transform="translate({slogan.group_x},{slogan.group_y})">   
-            <g id="slogan" style="font-size:{slogan.font_size};font-family:'{slogan.font}'">
+            <rect width='{background.width}' height='{background.height}' fill='{background.color}' />
+                <g id="logo-group">
+                    <image href="data:image/png;base64,{center_icon.png_base64}" x='{center_icon.x}' y='{center_icon.y}' width='{center_icon.width}' height='{center_icon.height}' transform=" scale({letter_icon.scale})"/>
+                    <g id="title-container" transform="translate({title.group_x},{title.group_y})">   
+                        <g id="title" style="font-size:{title.font_size};font-family:'{title.font}'">
+                        """
+        template += tab[1]
+    
+        template += """</g>
+            </g>
+                    <g id="slogan-container" transform="translate({slogan.group_x},{slogan.group_y})">   
+                        <g id="slogan" style="font-size:{slogan.font_size};font-family:'{slogan.font}'">
              
-    """
-    template += tab[2]
+         """
+        template += tab[2]
         
-    template += """</g>
+        template += """</g>
+            </g>
         </g>
-    </g>
         
-</svg>
-"""
+    </svg>
+    """
+    elif(splitted == True) : 
+        template = """
+        <svg xmlns='http://www.w3.org/2000/svg' width='{background.width}' height='{background.height}'>
+             <style>
+                @font-face {{
+                    font-family: '{title.font}';
+                    src: url(data:font/ttf;base64,{title.text_font_data_encoded}) format('truetype');
+                 }}
+                @font-face {{
+                    font-family: '{slogan.font}';
+                    src: url(data:font/ttf;base64,{slogan.text_font_data_encoded}) format('truetype');
+                }}
+            </style>
+    
+            <rect width='{background.width}' height='{background.height}' fill='{background.color}' />
+                <g id="logo-group">
+                    <image href="data:image/png;base64,{center_icon.png_base64}" x='{center_icon.x}' y='{center_icon.y}' width='{center_icon.width}' height='{center_icon.height}' transform=" scale({letter_icon.scale})"/>
+                    <g id="title_1-container" transform="translate({title_1.group_x},{title_1.group_y})">   
+                        <g id="title_1" style="font-size:{title_1.font_size};font-family:'{title_1.font}'">
+                        """
+        template += tab[1]
+    
+        template += """</g>
+            </g>
+                    <g id="title_2-container" transform="translate({title_2.group_x},{title_2.group_y})">   
+                        <g id="slogan" style="font-size:{title_2.font_size};font-family:'{title_2.font}'">
+             
+         """
+        template += tab[2]
+
+        template += """</g>
+            </g>
+                    <g id="slogan-container" transform="translate({slogan.group_x},{slogan.group_y})">   
+                        <g id="slogan" style="font-size:{slogan.font_size};font-family:'{slogan.font}'">
+             
+         """
+        template += tab[3]
+        
+        template += """</g>
+            </g>
+        </g>
+        
+    </svg>
+    """
     
     svg_content = template.format(
-            background=background,
-            title=title,
-            slogan=slogan, 
-            container_icon = container_icon,
-            center_icon = center_icon,
-            letter_icon = letter_icon
+        background=background,
+        title=title,
+        slogan=slogan, 
+        container_icon = container_icon,
+        center_icon = center_icon,
+        letter_icon = letter_icon,
+        title_1 = title_1,
+        title_2 = title_2
         )
+
     return svg_content
 
-
+  
 background = background_object.BackgroundObject(
     color = "#AAAAAA",
     width = 400,
@@ -72,6 +151,36 @@ background = background_object.BackgroundObject(
 
 title = text_object.TextObject(
     content="Coffee Inc.",
+    font_size= None,
+    font = "Aleo-Bold",
+    x = 0,
+    y = 0,
+    font_color= "#AA11AA",
+    text_font_data_encoded= None,
+    anchor = None,
+    scale_factor= None,
+    width = 0,
+    group_x = None,
+    group_y = None
+)
+
+title_1 = text_object.TextObject(
+    content="",
+    font_size= None,
+    font = "Aleo-Bold",
+    x = 0,
+    y = 0,
+    font_color= "#AA11AA",
+    text_font_data_encoded= None,
+    anchor = None,
+    scale_factor= None,
+    width = 0,
+    group_x = None,
+    group_y = None
+)
+
+title_2 = text_object.TextObject(
+    content="",
     font_size= None,
     font = "Aleo-Bold",
     x = 0,
@@ -135,8 +244,10 @@ letter_icon = icon_object.IconObject(
     scale = 1
     )
             
-center_icon.png_base64 = get_png_base64(center_icon.keyword, center_icon.color, 1, 2)
-tab = apply_layout(background, title, slogan,center_icon, 2)
-svg = generate_svg(background, title, slogan, container_icon, center_icon, letter_icon)    
+center_icon.png_base64 = get_png_base64(center_icon.keyword, center_icon.color, 0, 2) 
+splitted = title_splitter(title, title_1, title_2)
+tab = apply_layout(background, title, slogan,center_icon, 6, title_1, title_2)
+svg = generate_svg(background, title, slogan, container_icon, center_icon, letter_icon, splitted, title_1, title_2)    
+
 with open(Path(str(_DIR_DATA) + "/_output_svg.svg"), 'w') as f:  
     f.write(svg)
--- logo_maker/layout.py
+++ logo_maker/layout.py
@@ -15,7 +15,7 @@ def font_size_selector( text : TextObject, background : BackgroundObject, random
     return font_size
 
 def vertical_space_text(title : TextObject, slogan : TextObject):
-    return title.group_y + (title.font_size + slogan.font_size) / 2
+    return title.group_y + 1.1 * (title.font_size + slogan.font_size) / 2
 
 def font_size_to_scale(font_size : int, font_size_range : range, scale_range : range) -> float : 
     min_font_size, max_font_size = font_size_range
@@ -32,16 +32,16 @@ def font_size_to_scale(font_size : int, font_size_range : range, scale_range : r
 def icon_size_selector(background : BackgroundObject, random_size : float) -> float : 
     return background.height * 0.25 + random_size
     
-def apply_layout(background : BackgroundObject, title : TextObject , slogan : TextObject, center_icon : IconObject, layout : int ) -> int : 
+def apply_layout(background : BackgroundObject, title : TextObject , slogan : TextObject, center_icon : IconObject, layout : int, title_1 : TextObject, title_2 : TextObject) -> int : 
     random.seed(1)
     random_size = random.uniform(-2.5,2.5)
     font_size_range = (10, 70)  # This means that font sizes go from 10 to 20
     scale_range = (0.015, 0.1)  # This means that scales go from 1.0 to 2.0
-    title.font_size = font_size_selector(title, background, random_size)
-    title.scale_factor = font_size_to_scale(title.font_size, font_size_range, scale_range)
+    title.font_size = title_1.font_size = title_2.font_size = font_size_selector(title, background, random_size)
+    title.scale_factor = title_1.scale_factor = title_2.scale_factor = font_size_to_scale(title.font_size, font_size_range, scale_range)
     slogan.font_size = min(font_size_selector(slogan, background, random_size), title.font_size)
     slogan.scale_factor = font_size_to_scale(slogan.font_size, font_size_range,scale_range )
-    title.text_font_data_encoded = get_font_file(title)
+    title.text_font_data_encoded = title_1.text_font_data_encoded = title_2.text_font_data_encoded = get_font_file(title)
     slogan.text_font_data_encoded = get_font_file(slogan)
 
     if layout == 1 :
@@ -80,15 +80,76 @@ def apply_layout(background : BackgroundObject, title : TextObject , slogan : Te
         tab = [None]*3
         title_paths = string_to_svg_paths(title)
         slogan_paths = string_to_svg_paths(slogan)
-        title.group_x = 200 - title.width * 0.5
-        title.group_y = 150 + title.font_size
-        slogan.group_x = 200 - slogan.width * 0.5
+        title.group_x = 130
+        title.group_y = 150 
+        slogan.group_x = 130
         slogan.group_y = vertical_space_text(title, slogan)
         center_icon.height = icon_size_selector(background, 2 * random_size)
         center_icon.width = center_icon.height
-        center_icon.x= 200 - center_icon.width/2
-        center_icon.y = 150 - 1.1 * center_icon.height
-        tab[0] = 2
+        center_icon.x= title.group_x - 1.2 * center_icon.width
+        center_icon.y = title.group_y - 0.5 * center_icon.height
+        tab[0] = 3
         tab[1] = title_paths
         tab[2] = slogan_paths
-        return 2
+        return tab
+    
+    elif layout == 4 :
+        tab = [None]*4
+        title_1_paths = string_to_svg_paths(title_1)
+        title_2_paths = string_to_svg_paths(title_2)
+        slogan_paths = string_to_svg_paths(slogan)
+        center_icon.x= -10000
+        center_icon.y = -10000
+        title_1.group_x =  200 - title_1.width * 0.5
+        title_2.group_x = 200 - title_2.width * 0.5
+        title_1.group_y = 150 
+        title_2.group_y = vertical_space_text(title_1, title_2)
+        slogan.group_x = 200 - slogan.width * 0.5
+        slogan.group_y = vertical_space_text(title_2, slogan)
+        tab[0] = 4
+        tab[1] = title_1_paths
+        tab[2] = title_2_paths
+        tab[3] = slogan_paths
+        return tab
+    
+    elif layout == 5 : 
+        tab = [None]*4
+        title_1_paths = string_to_svg_paths(title_1)
+        title_2_paths = string_to_svg_paths(title_2)
+        slogan_paths = string_to_svg_paths(slogan)
+        title_1.group_x =  200 - title_1.width * 0.5
+        title_2.group_x = 200 - title_2.width * 0.5
+        title_1.group_y = 150 + 0.5 * title_1.font_size
+        title_2.group_y = vertical_space_text(title_1, title_2)
+        slogan.group_x = 200 - slogan.width * 0.5
+        slogan.group_y = vertical_space_text(title_2, slogan)
+        center_icon.height = icon_size_selector(background, 2 * random_size)
+        center_icon.width = center_icon.height
+        center_icon.x= 200 - center_icon.width/2
+        center_icon.y = title_1.group_y - 1.5 * center_icon.height
+        tab[0] = 5
+        tab[1] = title_1_paths
+        tab[2] = title_2_paths
+        tab[3] = slogan_paths
+        return tab
+    
+    elif layout == 6 : 
+        tab = [None]*4
+        title_1_paths = string_to_svg_paths(title_1)
+        title_2_paths = string_to_svg_paths(title_2)
+        slogan_paths = string_to_svg_paths(slogan)
+        center_icon.height = icon_size_selector(background, 2 * random_size)
+        center_icon.width = center_icon.height
+        center_icon.x= 200 - center_icon.width/2 + (title_1.width - title_2.width) * 0.5
+        center_icon.y = 150 - center_icon.height/2
+        title_1.group_x =  200 - title_1.width - center_icon.width / 2 + (title_1.width - title_2.width) * 0.5
+        title_2.group_x = 200 + center_icon.width/2 + (title_1.width - title_2.width) * 0.5
+        title_1.group_y = title_2.group_y = 150 + 0.5 * title_1.font_size
+        slogan.group_x = 200 - slogan.width * 0.5
+        slogan.group_y = vertical_space_text(title_2, slogan)
+
+        tab[0] = 6
+        tab[1] = title_1_paths
+        tab[2] = title_2_paths
+        tab[3] = slogan_paths
+        return tab
